17a107fc9152dfde61f54b363e843503bbea64f2
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-01-26 00:01:21 -0300

    development and major improvements

--- consoleScript.js
+++ consoleScript.js
@@ -1,13 +1,24 @@
-Array.from(document.querySelectorAll('[id^="BOOK"]')).forEach((e) => {
-  console.log(
-    e.querySelector('[id^="lblAtivoNome"]').textContent.match(/\s([^ ]+)$/)[0]
-  );
-  Array.from(e.querySelectorAll('[id^="tblGridOfertas"]')).forEach((ee) =>
-    console.log(ee.querySelector('[class^="onClickBook"] .TD_col3').textContent)
-  );
-});
+const outputArray = () =>
+  Array.from(document.querySelectorAll('[id^="BOOK"]'))
+    .map((e) => {
+      return {
+        strike: e
+          .querySelector('[id^="lblAtivoNome"]')
+          .textContent.match(/\s([^ ]+)$/)[0]
+          .trim(),
+        buyPrice: e.querySelector(
+          '[id^="tblGridOfertasCompra"] .onClickBook .TD_col3'
+        ).textContent,
+        sellPrice: e.querySelector(
+          '[id^="tblGridOfertasVenda"] .onClickBook .TD_col3'
+        ).textContent,
+      };
+    })
+    .sort((a, b) => parseFloat(a.strike) - parseFloat(b.strike));
 
-fetch("http://127.0.0.1:8000/post/", {
-  method: "POST",
-  body: JSON.stringify({ eae: "meo" }),
-});
+setInterval(() => {
+  fetch("http://127.0.0.1:8000/post/", {
+    method: "POST",
+    body: JSON.stringify({ optionsData: outputArray() }),
+  });
+}, 10000);
