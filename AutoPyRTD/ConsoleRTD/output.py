5d9949467b28f71fcb5adbbbb8c00381679f6b85
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-01-30 17:42:27 -0300

    reliable lists generation, now working on the output

--- AutoPyRTD/ConsoleRTD/output.py
+++ AutoPyRTD/ConsoleRTD/output.py
@@ -1,8 +1,8 @@
 import time
 
 def toConsole(o):
-#     LINE_UP = '\033[1A'
-#     LINE_CLEAR = '\x1b[2K'
+    LINE_UP = '\033[1A'
+    LINE_CLEAR = '\x1b[2K'
 
 #     print("Line 1: Static content")
 #     print("-")
@@ -16,7 +16,6 @@ def toConsole(o):
 #         print(message)
 #         print(message)
 # toConsole()
-    print('\x1b[2J\x1b[H')
-    [print (k) for k in o]
+    [(print(LINE_CLEAR), print(LINE_UP)) for _ in range(144)]
 
     
