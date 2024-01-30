e8b3f76e0e20cbf9569fc0676b52a9436fefeb54
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-01-30 15:24:34 -0300

    filter is functional but the overall information is not reliable yet

--- AutoPyRTD/ConsoleRTD/output.py
+++ AutoPyRTD/ConsoleRTD/output.py
@@ -1,20 +1,22 @@
 import time
 
 def toConsole(o):
-    # LINE_UP = '\033[1A'
-    # LINE_CLEAR = '\x1b[2K'
+#     LINE_UP = '\033[1A'
+#     LINE_CLEAR = '\x1b[2K'
 
-    # print("Line 1: Static content")
-    # print("-")
-    # print("-")
+#     print("Line 1: Static content")
+#     print("-")
+#     print("-")
 
-    # for color_code in range(31, 40):
-    #     message = f"\033[{color_code}m  Text with color {color_code}\033[0m"
-    #     time.sleep(1)
-    #     print(LINE_UP, end=LINE_CLEAR)
-    #     print(LINE_UP, end=LINE_CLEAR)
-    #     print(message)
-    #     print(message)
-
-    # # Print a new line to avoid overwriting the last line
+#     for color_code in range(29, 37):
+#         message = f"\033[{color_code}m  Text with color {color_code}\033[0m"
+#         time.sleep(1)
+#         print(LINE_UP, end=LINE_CLEAR)
+#         print(LINE_UP, end=LINE_CLEAR)
+#         print(message)
+#         print(message)
+# toConsole()
+    print('\x1b[2J\x1b[H')
     [print (k) for k in o]
+
+    
