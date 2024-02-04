1d532f426ef1bd5e8629707aeb6814cf9f7546aa
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-02-04 00:23:03 -0300

    added entry prices for lock operations

--- AutoPyRTD/DataGeneration/process.py
+++ AutoPyRTD/DataGeneration/process.py
@@ -4,12 +4,12 @@ callCodes = ['A','B','C','D','E','F','G','H','I','J','K','L']
 
 def getLockOutput(data, type):
     all_lock_combinations = [
-        (i['strike'],i['code']+"("+i['strike']+")",ii['code']+"("+ii['strike']+")",
+        (i['strike'],i['code']+"("+i['strike']+")",i['sellPrice'],ii['code']+"("+ii['strike']+")",ii['buyPrice'],
         (float(i['sellPrice'])-float(ii['buyPrice']))/(float(ii['strike'])-float(i['strike']))) 
         for i in data for ii in data if ii['code'] != i['code'] and ii['strike'] >= i['strike']
     ] if type == 'CALL' else [
-        (ii['strike'],ii['code']+"("+ii['strike']+")",i['code']+"("+i['strike']+")",
-        (float(ii['sellPrice'])-float(i['buyPrice']))/(float(ii['strike'])-float(i['strike']))) 
+        (ii['strike'],ii['code']+"("+ii['strike']+")",ii['sellPrice'],i['code']+"("+i['strike']+")",i['buyPrice'],
+        (float(ii['sellPrice'])-float(i['buyPrice']))/(float(ii['strike'])-float(i['strike'])))
         for i in data for ii in data if ii['code'] != i['code'] and ii['strike'] >= i['strike']
     ]
     # return list(filter(lambda l: l[3] <= 0.25 ,sorted(all_lock_combinations, key=lambda x: x[0],reverse=False if type == 'CALL' else True)))
